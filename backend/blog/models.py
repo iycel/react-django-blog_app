8699cba38fd59d0b6ceb716547f05a8c2d3ed65a
iycel <ismailycel@gmail.com>
2022-01-09 23:37:34 +0300

    backend

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from datetime import datetime
+from django.template.defaultfilters import slugify
+class BlogPost(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+    CATEGORIES = {
+        ('world', 'WORLD'),
+        ('environmnet', 'ENVIRONMENT'),
+        ('technology', 'TECHNOLOGY'),
+        ('design', 'DESIGN'),
+        ('culture', 'CULTURE'),
+        ('business', 'BUSINESS'),
+        ('politics', 'POLITICS'),
+        ('opinion', 'OPINION'),
+        ('science', 'SCIENCE'),
+        ('health', 'HEALTH'),
+        ('style', 'STYLE'),
+        ('travel', 'TRAVEL'),
+    }
+    category = models.CharField(max_length=50, choices=CATEGORIES, default='WORLD')
+    thumbnail = models.ImageField(upload_to='photos/%Y/%m/%d/')
 
-# Create your models here.
+
